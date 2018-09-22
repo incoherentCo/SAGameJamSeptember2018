29fef094416a3affedec0f55aae12fc0a57b7426
HardusvanZyl96 <38581415+HardusvanZyl96@users.noreply.github.com>
2018-09-22 12:30:17 +0200

    progress

---
+++ Assets/Scripts/Managers/Module_Manager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Module_Manager : MonoBehaviour {
+
+    IDictionary<Character, ModuleAction[]> CharActionIndex;
+
+    private void Start()
+    {
+        CharActionIndex = new Dictionary<Character, ModuleAction[]>()
+    }
+
+
+    public ModuleAction[] FindAllModulesInHierarchy(GameObject character)
+    {
+        return GetComponentsInChildren<ModuleAction>();
+    }
+}
